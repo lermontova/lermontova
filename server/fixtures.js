00ca5632c381a06f6f0d7f555225a5cd17bc46e6
Valery <v.tsybulko@favbet.com>
2015-07-21 16:18:43 +0300

    server

---
+++ server/fixtures.js
@@ -0,0 +1,17 @@
+ 
+if (Posts.find().count() === 0) {
+  Posts.insert({
+    title: 'IAMX',
+    url: 'https://www.youtube.com/playlist?list=PLGKrghb-9eAB4x84PNSQukU9VaRMbvQu6'
+  });
+
+  Posts.insert({
+    title: 'IAMX',
+    url: 'https://www.youtube.com/playlist?list=PLGKrghb-9eAB4x84PNSQukU9VaRMbvQu6'
+  });
+
+  Posts.insert({
+    title: 'IAMX',
+    url: 'https://www.youtube.com/playlist?list=PLGKrghb-9eAB4x84PNSQukU9VaRMbvQu6'
+  });
+}
