7b872603aebdff81910caad035d9d38679815c79
Valery <v.tsybulko@favbet.com>
2015-07-21 16:17:32 +0300

    js

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,18 +1,4 @@
- 
-var postsData = [
-  {
-    title: 'IAMX',
-    url: 'https://www.youtube.com/playlist?list=PLGKrghb-9eAB4x84PNSQukU9VaRMbvQu6'
-  }, 
-  {
-    title: 'IAMX',
-    url: 'https://www.youtube.com/playlist?list=PLGKrghb-9eAB4x84PNSQukU9VaRMbvQu6'
-  }, 
-  {
-    title: 'IAMX',
-    url: 'https://www.youtube.com/playlist?list=PLGKrghb-9eAB4x84PNSQukU9VaRMbvQu6'
-  }
-];
-Template.postsList.helpers({
-  posts: postsData
-});
+Template.postsList.helpers({  
+	posts: function() {
+	    return Posts.find();  
+	}});
