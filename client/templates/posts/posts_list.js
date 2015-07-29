11bdd890375fba8d81001e1abf71c8c677eac5ef
Valery <v.tsybulko@favbet.com>
2015-07-29 11:52:23 +0300

    method of new post2

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,2 +1,6 @@
  
-Template.postsList.helpers({  posts: function() {    return Posts.find();  }});
+Template.postsList.helpers({  
+	posts: function() {
+	    return Posts.find({}, {sort:{submitted: -1}});
+	    	}
+	   });
